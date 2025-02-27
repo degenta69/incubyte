2deb20bb0475d105fb32bb87cc43bf22143e4aed
dipanshSaxenaVivo <dipansh.saxena@vivo.ooo>
2025-02-27 22:03:00 +0530

    add comma separated  numbers

--- stringCalculator.test.ts
+++ stringCalculator.test.ts
@@ -6,4 +6,11 @@ test("returns 0 for an empty string", () => {
 
 test("returns the number itself when only one number is provided", () => {
   expect(add("1")).toBe(1);
+  expect(add("5")).toBe(5);
+  expect(add("9")).toBe(9);
 });
+
+test("returns the sum of two comma-separated numbers", () => {
+  expect(add("1,5")).toBe(6);
+});
+
--- stringCalculator.ts
+++ stringCalculator.ts
@@ -1,4 +1,7 @@
 export function add(numbers: string): number {
   if (numbers === "") return 0;
-  return parseInt(numbers);
+  // return parseInt(numbers);
+
+  const numArray = numbers.split(",").map(Number);
+  return numArray.reduce((sum, num) => sum + num, 0);
 }
